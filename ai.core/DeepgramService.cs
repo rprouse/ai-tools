b2ced69f2de21d60ef2835a7b8837858f440b055
Rob Prouse <rob.prouse@aspiresoftware.com>
2025-04-07 13:10:47 -0400

    Initial commit

---
+++ ai.core/DeepgramService.cs
@@ -0,0 +1,41 @@
+﻿using Deepgram;
+using Deepgram.Models.Listen.v1.REST;
+
+namespace ai.core;
+
+public class DeepgramService
+{
+    public static async Task<string> TranscribeAudioAsync(string audioFilePath, string apiKey)
+    {
+        try
+        {
+            // Initialize Library with default logging
+            // Normal logging is "Info" level
+            Library.Initialize();
+
+            // use the client factory with a API Key set with the "DEEPGRAM_API_KEY" environment variable
+            var deepgramClient = ClientFactory.CreateListenRESTClient(apiKey);
+
+            // check to see if the file exists
+            if (!File.Exists(audioFilePath))
+            {
+                throw new FileNotFoundException("File not found", audioFilePath);
+            }
+
+            var audioData = File.ReadAllBytes(audioFilePath);
+            var response = await deepgramClient.TranscribeFile(
+                audioData,
+                new PreRecordedSchema()
+                {
+                    Model = "nova-3",
+                });
+
+            return response.ToString();
+        }
+        finally
+        {
+            // Teardown Library
+            Library.Terminate();
+        }
+    }
+}
---
+++ tools/transcribe/Program.cs
@@ -0,0 +1,12 @@
+﻿var key = Environment.GetEnvironmentVariable("DEEPGRAM_API_KEY");
+if (string.IsNullOrEmpty(key))
+{
+    throw new InvalidOperationException("The DEEPGRAM_API_KEY environment variable is not set.");
+}
+var file = @"D:\src\Manos\DentaScribe\DentaScribe.Server\Audio\scottish_dentist.mp3";
+
+string response = await ai.core.DeepgramService.TranscribeAudioAsync(file, key);
+
+Console.WriteLine(response);
+
+
